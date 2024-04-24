395c51dffb64933eea3034ab637886b271bb4525
Jakob <202007550@post.au.dk>
2024-04-24 13:28:37 +0200

    Simulate commands

--- catkin_ws/devel/share/gennodejs/ros/turtlebot3_msgs/msg/_index.js
+++ catkin_ws/devel/share/gennodejs/ros/turtlebot3_msgs/msg/_index.js
@@ -1,12 +1,12 @@
 
 "use strict";
 
-let VersionInfo = require('./VersionInfo.js');
 let SensorState = require('./SensorState.js');
+let VersionInfo = require('./VersionInfo.js');
 let Sound = require('./Sound.js');
 
 module.exports = {
-  VersionInfo: VersionInfo,
   SensorState: SensorState,
+  VersionInfo: VersionInfo,
   Sound: Sound,
 };
