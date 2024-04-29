0b39d811153068ffe1d05930f75a35876180acda
Jakob <202007550@post.au.dk>
2024-04-29 10:28:14 +0000

    finish readfromrgb

--- catkin_ws/src/turtlebot3/turtlebot3_example/nodes/readFromRGB.py
+++ catkin_ws/src/turtlebot3/turtlebot3_example/nodes/readFromRGB.py
@@ -10,15 +10,26 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(led,GPIO.OUT)
 
-def mean(a, b, c):
-        return (a + b + c) / 3
+def mean_lists(a, b, c):
+        liste = []
+        for i in range(len(a)):
+                liste.append((a[i] + b[i] + c[i]) / 3)
+        return sum(liste) / len(liste)
 
 def detectVictim():
-        green = (bus.read_byte_data(0x44, 0x09) + bus.read_byte_data(0x44, 0x0A)*255) / 255
-        red = (bus.read_byte_data(0x44, 0x0B) + bus.read_byte_data(0x44, 0x0C)*255) / 255
-        blue = (bus.read_byte_data(0x44, 0x0D) + bus.read_byte_data(0x44, 0x0E)*255) / 255 * 1.75
+        reds = []
+        greens = []
+        blues = []
 
-        if mean(red, green, blue) < 3:
+        for i in range(10):
+                green = (bus.read_byte_data(0x44, 0x09) + bus.read_byte_data(0x44, 0x0A)*255) / 255
+                red = (bus.read_byte_data(0x44, 0x0B) + bus.read_byte_data(0x44, 0x0C)*255) / 255
+                blue = (bus.read_byte_data(0x44, 0x0D) + bus.read_byte_data(0x44, 0x0E)*255) / 255 * 1.75
+                greens.append(green)
+                reds.append(red)
+                blues.append(blue)
+
+        if mean_lists(reds, greens, blues) < 3:
                 return True
         return False
 
@@ -27,3 +38,5 @@ def blinkLED():
         time.sleep(1)
         GPIO.output(led,GPIO.LOW)
 
+if detectVictim():
+        print("Victim detected")
