4a53aff6b0272c0ed7d60dc1dfd0f2f77ea7e3ef
Jakob <202007550@post.au.dk>
2024-05-08 13:25:28 +0000

    RGB reader is now satisfactory enough

--- catkin_ws/src/turtlebot3/turtlebot3_example/nodes/readFromRGB.py
+++ catkin_ws/src/turtlebot3/turtlebot3_example/nodes/readFromRGB.py
@@ -11,40 +11,55 @@ GPIO.setwarnings(False)
 GPIO.setup(led,GPIO.OUT)
 
 def mean_list(a):
-        return sum(a) / len(a)
+    return sum(a) / len(a)
 
 def mean_lists(a, b, c):
-        liste = []
-        for i in range(len(a)):
-                liste.append((a[i] + b[i] + c[i]) / 3)
-        return sum(liste) / len(liste)
+    liste = []
+    for i in range(len(a)):
+        liste.append((a[i] + b[i] + c[i]) / 3)
+    return sum(liste) / len(liste)
 
 def detectVictim():
-        reds = []
-        greens = []
-        blues = []
+    reds = []
+    greens = []
+    blues = []
 
-        for i in range(10):
-                green = (bus.read_byte_data(0x44, 0x09) + bus.read_byte_data(0x44, 0x0A)*255) / 255
-                red = (bus.read_byte_data(0x44, 0x0B) + bus.read_byte_data(0x44, 0x0C)*255) / 255
-                blue = (bus.read_byte_data(0x44, 0x0D) + bus.read_byte_data(0x44, 0x0E)*255) / 255 * 1.75
-                greens.append(green)
-                reds.append(red)
-                blues.append(blue)
+    for i in range(60):
+        #time.sleep(0.001)
+        green = (bus.read_byte_data(0x44, 0x09) + bus.read_byte_data(0x44, 0x0A)*255) / 255
+        red = (bus.read_byte_data(0x44, 0x0B) + bus.read_byte_data(0x44, 0x0C)*255) / 255
+        blue = (bus.read_byte_data(0x44, 0x0D) + bus.read_byte_data(0x44, 0x0E)*255) / 255 * 1.75
+        greens.append(green)
+        reds.append(red)
+        blues.append(blue)
 
-        if mean_list(reds) > mean_list(greens) + 1.5 and mean_list(reds) > mean_list(blues) + 1.5:
-                return True
+    red_mean, green_mean, blue_mean = mean_list(reds), mean_list(greens), mean_list(blues)
 
-        return False
+    #print(f'Red: {red_mean}, Green: {green_mean}, Blue: {blue_mean}')
+
+    if red_mean > green_mean + blue_mean - ((red_mean + green_mean + blue_mean) / 9):
+        return True
+
+    return False
 
 def LEDon():
-        GPIO.output(led,GPIO.LOW)
+    GPIO.output(led,GPIO.LOW)
 
 def LEDoff():
-        GPIO.output(led,GPIO.HIGH)
+    GPIO.output(led,GPIO.HIGH)
 
 if __name__ == "__main__":
+    try:
+        victims = 0
         while True:
-                if detectVictim():
-                        print("Victim detected")
-                        #blinkLED()
+            LEDoff()
+                    
+            if detectVictim():
+                victims += 1
+                print("Victim detected")
+                LEDon()
+
+    except KeyboardInterrupt:
+        pass
+        #print(f"Victims: {victims}")
+            
